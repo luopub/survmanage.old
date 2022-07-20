7360aeb8c0b786115bb610f5fa4746b8ef14f006
luopub <22909077@qq.com>
2022-07-20 23:43:22 +0800

    add login

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,11 @@ from algorithm.views import router as algorithm_router
 from channel.views import router as channel_router
 from push.views import router as push_router
 from system.views import router as system_router
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
+    path('login/', obtain_auth_token),
     path('alert/', include(alert_router.urls)),
     path('algorithm/', include(algorithm_router.urls)),
     path('channel/', include(channel_router.urls)),
