2619c216b5c02c2d7b03e1d566c87ef69628bd9a
luopub <22909077@qq.com>
2022-08-06 01:20:29 +0800

    add

--- channel/views.py
+++ channel/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 
 from utils.rest_mixins import GroupbyMixin
 from utils.rest_utils import MyModelViewSet, SimpleViewSetBase
+from cameradaemon.image_client import ImageClient
+from cameradaemon.image_server_code import *
 
 from .models import Channel, ChannelAlgorithm
 
@@ -28,6 +30,16 @@ class ChannelViewSet(GroupbyMixin, MyModelViewSet, metaclass=SimpleViewSetBase):
         channel_ids = request.GET.get('channel_ids').split(',')
         images = {id_: (int(id_) % 2 == 0) for id_ in channel_ids}
         return Response(images)
+
+    @action(detail=True, methods=['get'])
+    def get_real_time_image(self, request, pk):
+        channel = self.model.objects.get(id=pk)
+
+        res = ImageClient(IMG_CMD_GET_LATEST_IMAGE, cno=channel.cno).do_request()
+        if res and res['code'] == IMG_CODE_SUCCESS:
+            return Response({'filename': res['data']['filename']})
+        else:
+            return Response({})
 
 
 class ChannelAlgorithmViewSet(GroupbyMixin, MyModelViewSet, metaclass=SimpleViewSetBase):
