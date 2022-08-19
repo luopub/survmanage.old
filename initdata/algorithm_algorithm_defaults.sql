64359e16aef74dbcfa889537d22532973523e291
luopub <22909077@qq.com>
2022-08-20 02:18:56 +0800

    add algorithm_algorithm_defaults.sql

---
+++ initdata/algorithm_algorithm_defaults.sql
@@ -0,0 +1,14 @@
+INSERT INTO survmanage.algorithm_algorithm (name,name_ch) VALUES
+	 ('safetyhelmet','安全帽'),
+	 ('vest','反光衣'),
+	 ('fire','火焰'),
+	 ('motor','摩托车'),
+	 ('electomotor','电瓶车'),
+	 ('cigarette','吸烟'),
+	 ('human','人体'),
+	 ('plate','车牌'),
+	 ('mobile','电话'),
+	 ('vehicle','车辆');
+INSERT INTO survmanage.algorithm_algorithm (name,name_ch) VALUES
+	 ('animal','动物'),
+	 ('facemask','口罩');
