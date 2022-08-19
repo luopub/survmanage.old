cfd1f0fdfffa1b6bffe847bcfbd6838f8fc5320e
myaibox <22909077@qq.com>
2022-08-19 02:58:12 -0700

    docker network

--- survmanage/settings.py
+++ survmanage/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'survmanage',
         'USER': 'root',
         'PASSWORD': '12345678',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql' if os.path.exists('/.dockerenv') else '127.0.0.1', # If in docker use mysql's network name
         'PORT': 3306,
         'charset': 'utf8mb4',
     },
