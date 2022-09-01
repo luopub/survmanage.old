0a41389f469b31aa7df4bad4ce00a85443691e5b
luopub <22909077@qq.com>
2022-09-01 20:31:23 +0800

    add get_default for constant defaults

--- algorithm/views.py
+++ algorithm/views.py
@@ -16,6 +16,22 @@ class AlgorithmViewSet(GroupbyMixin, MyModelViewSet, metaclass=SimpleViewSetBase
 class AlgorithmParametersDefaultViewSet(GroupbyMixin, MyModelViewSet, metaclass=SimpleViewSetBase):
     model = AlgorithmDefaultParameters
 
+    @action(detail=False, methods=['get'])
+    def get_default(self, request):
+        """
+        由于取消了默认参数设置，这里返回固定值
+        """
+        value = [
+            {
+                "alert_interval": 30,
+                "alert_threshold": 0.5,
+                "alert_times": "[{\"enabled\":true,\"segs\":[[0,1440]]},{\"enabled\":true,\"segs\":[[0,1440]]},{\"enabled\":true,\"segs\":[[0,1440]]},{\"enabled\":true,\"segs\":[[0,1440]]},{\"enabled\":true,\"segs\":[[0,1440]]},{\"enabled\":true,\"segs\":[[0,1440]]},{\"enabled\":true,\"segs\":[[0,1440]]}]",
+                "analyze_interval": 1000,
+                "id": 10
+            }
+        ]
+        return Response(value)
+
 
 router = routers.DefaultRouter()
 router.register('algorithms', AlgorithmViewSet)
