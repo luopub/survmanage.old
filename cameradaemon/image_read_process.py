20af6c6510ac0c6169e6dda1ed9effd7ff8a876b
luopub <22909077@qq.com>
2022-09-05 00:37:41 +0800

    update docker

--- cameradaemon/image_read_process.py
+++ cameradaemon/image_read_process.py
@@ -11,6 +11,10 @@ from .image_client import ImageClient
 from .image_server_code import *
 from .utils import save_raw_frame
 
+from utils.logutils import get_logger
+
+logger = get_logger('image_read_process')
+
 
 DEFAULT_DETECT_TICK = 500  # 默认检测500毫秒为单位
 DEFAULT_LATEST_IMAGE_INTERVAL = 1000  # 默认保存最新图片间隔
@@ -33,14 +37,14 @@ class ImageStreamProcess(Process):
 
     def process_loop(self):
         cno = self.cno
-        print(f'{cno}-Process to write: {os.getpid()}')
+        logger.info(f'{cno}-Process to write: {os.getpid()}')
         while True:
             camera = self.camera.value
             if camera:
                 t1 = time.time()
-                print(f'{cno}-Start VideoCapture', camera)
+                logger.info(f'{cno}-Start VideoCapture: {camera}')
                 cap = cv.VideoCapture(camera)
-                print(f'{cno}-Time used for start camera: ', time.time() - t1)
+                logger.info(f'{cno}-Time used for start camera: {time.time() - t1}')
             else:
                 cap = None
 
@@ -75,7 +79,7 @@ class ImageStreamProcess(Process):
                         if camera != self.camera.value:
                             break
 
-            print(f'{cno}-camera is offline!')
+            logger.info(f'{cno}-camera is offline!')
             self.online.value = 0
             # Wait for some time to try again
             if cap:
@@ -95,8 +99,8 @@ class DetectionModel:
         if self.model_path:
             # 非常奇怪：如果没有这两句显示，模型初始化会失败！！！
             import torch
-            print('torch.cuda.is_available()', torch.cuda.is_available())
-            print('torch.cuda.device_count()', torch.cuda.device_count())
+            logger.info(f'torch.cuda.is_available(): {torch.cuda.is_available()}')
+            logger.info(f'torch.cuda.device_count(): {torch.cuda.device_count()}')
             self.model = YOLOv5(self.model_path, device=self.model_device)
 
     def set_params(self, cas):
@@ -223,7 +227,7 @@ class DetectionModel:
         if self.model_device.startswith('cuda'):
             results.pred[0] = results.pred[0].cpu()
 
-        print(f'{cno}-predict_single_frame: found {len(results.pred[0])} objects: {[(results.names[int(p[-1])], p[-2]) for p in results.pred[0]]}')
+        logger.info(f'{cno}-predict_single_frame: found {len(results.pred[0])} objects: {[(results.names[int(p[-1])], p[-2]) for p in results.pred[0]]}')
 
         # 取得分类索引对应的阈值和roi区域
         thresholds = {results.names.index(ca['model_name']): ca['alert_threshold'] for ca in avail_cas}
@@ -248,7 +252,7 @@ class DetectionModel:
             # 如果没有合适结果，直接返回
             return
 
-        print(f'{cno}-predict_single_frame: remain {len(results.pred[0])} objects: {[results.names[int(p[-1])] for p in results.pred[0]]}')
+        logger.info(f'{cno}-predict_single_frame: remain {len(results.pred[0])} objects: {[results.names[int(p[-1])] for p in results.pred[0]]}')
 
         # 首先保存未标注的图片
         img_unmark = save_raw_frame(results.imgs[0], cno=cno, cvt_color=False)
@@ -292,7 +296,7 @@ class DetectionModel:
                         ca['last_alert_time'] = time.time()
                         break
 
-            print(f'{cno}-predicts', predicts)
+            logger.info(f'{cno}-predicts: , {predicts}')
 
             # 保存报警信息
             ImageClient(IMG_CMD_OBJECT_DETECTED, cno=cno, img_unmark=img_unmark, img=img, predicts=predicts).do_request(wait_result=False)
@@ -316,7 +320,7 @@ class ImageConsumeProcess(Process):
 
     # 在缓冲栈中读取数据:
     def process_loop(self):
-        print(f'{self.cno}-Process to read: %s' % os.getpid())
+        logger.info(f'{self.cno}-Process to read: %s' % os.getpid())
         self.model.init_model()
         # 开始时间
         t1 = time.time()  # 最新图片保存定时
