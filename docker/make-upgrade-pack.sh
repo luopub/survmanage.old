6ff8b6f19f8a5b04a091f310dc78f484b4e0a1ff
luopub <22909077@qq.com>
2022-09-12 05:24:51 +0800

    update make-upgrade-pack

--- docker/make-upgrade-pack.sh
+++ docker/make-upgrade-pack.sh
@@ -45,7 +45,7 @@ sudo cd "${working_dir}"
 sudo docker save survmanage:latest -o survmanage-latest.tar && sudo chmod +r survmanage-latest.tar
 sudo docker save survmanagenginx:latest -o survmanagenginx-latest.tar && sudo chmod +r survmanagenginx-latest.tar
 sudo docker save imageserver:latest -o imageserver-latest.tar && sudo chmod +r imageserver-latest.tar
-sudo cp -R /docker .
+#sudo cp -R /docker .
 sudo cp /etc/rc.local .
 
 # Then pack the directory
