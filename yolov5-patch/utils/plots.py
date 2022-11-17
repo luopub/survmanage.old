3ddebd24673eb16e55a79221f65c874f3bbae22d
luopub <22909077@qq.com>
2022-11-17 17:16:14 +0800

    update plots.py

--- yolov5-patch/utils/plots.py
+++ yolov5-patch/utils/plots.py
@@ -88,6 +88,7 @@ class Annotator:
         self.lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)  # line width
 
     def box_label(self, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
+        txt_color = color
         # Add one xyxy box to image with label
         if self.pil or not is_ascii(label):
             self.draw.rectangle(box, width=self.lw, outline=color)  # box
